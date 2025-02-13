7b4866eb0fa854768aa200717372407c7c5d7cb6
Nikpesu <92652074+Nikpesu@users.noreply.github.com>
2025-02-13 22:45:12 +0100

    Update setup.sh
    implemented needed changes, and added so .uf2 file copies next to script

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ echo -e "\n\tStep 3.\n"
 apt install build-essential
 
 echo -e "\n\tStep 4\n"
-apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
+sudo apt install cmake python3 build-essential gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
 git clone https://github.com/raspberrypi/pico-sdk.git
 rm ./RP2040MacropadHidSample/src/pico_sdk_import.cmake
 cp ./pico-sdk/external/pico_sdk_import.cmake ./RP2040MacropadHidSample/src/pico_sdk_import.cmake
@@ -30,10 +30,10 @@ cd ..
 
 echo -e "\n\tStep 6.\n"
 git clone https://github.com/hathach/tinyusb.git
-rm ./pico-sdk/lib/tinyusb/src/class/hid/hid.h
-rm ./pico-sdk/lib/tinyusb/src/class/hid/hid_device.h
-cp ./tinyusb/src/class/hid/hid.h ./pico-sdk/lib/tinyusb/src/class/hid/hid.h
-cp ./tinyusb/src/class/hid/hid_device.h  ./pico-sdk/lib/tinyusb/src/class/hid/hid_device.h
+cd ./pico-sdk/lib/tinyusb
+git checkout master
+git pull
+cd ../../..
 
 echo -e "\n\tStep 7.\n"
 cd ./RP2040MacropadHidSample/src/
@@ -44,9 +44,11 @@ cmake -DPICO_BOARD=adafruit_macropad_rp2040 ..
 
 echo -e "\n\tStep 8.\n"
 make
+cd ../../..
+cp ./RP2040MacropadHidSample/src/build/macropad.uf2 ./macropad.uf2
 
 echo -e "\n\tThats it,\n"
 echo -e "  9. On the Macropad, while holding down the rotary encoder b utton, press the reset button on the left side of the board, right under the OLED screen. Macropad device should show up as a removable device RPI-RP2 (DriveLetter:)"
-echo -e "  10. Open an Explorer window to the build directory, and copy macropad.uf2 to the Macropad removeable device. Device should automatically remove itself and reboot."
+echo -e "  10. Open an Explorer window to the build directory (or where this script is), and copy macropad.uf2 to the Macropad removeable device. Device should automatically remove itself and reboot."
 echo -e "  11. Open Settings > Personalization > Dynamic Lighting, and see the device come up.\n"
 
